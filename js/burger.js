bc6f197e37afc18e9ed6be3297a58e4b521c0d22
Syuanruei <tyopu289@gmail.com>
2023-08-28 19:29:02 +0800

    no message

---
+++ js/burger.js
@@ -0,0 +1,7 @@
+// let menu_icon_box = document.querySelector(".menu_icon_box");
+// let burger = document.querySelector("#burger");
+
+// menu_icon_box.onclick = function() {
+//     menu_icon_box.classList.toggle('active');
+//     burger.classList.toggle('active');
+// }
